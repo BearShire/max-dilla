dced71e70a3f91fc4a23a9036841ee5d160f3490
BearShire <dudek21_1994@o2.pl>
2016-09-18 20:40:57 +0200

    New functions, ver 0.4
    18.09.2016 0.4
    -Poprawiony błąd krytyczny - debilny warunek na końcu while'a w gra();
    -Poprawiona implementacja sprawdzania poprawności pliku save w przypadku
    kontynuacji gry
    -obcięcie paru zmiennych, lekkie uproszczenie kodu
    -responsywne menu główne i menu w głównej części gry
    -zmiana nazw funkcji na anglojęzyczne
    -funckja gameover & yourdead
    -dodany szkielet sklepu
    -dodany szpital z funkcją leczenia zależną od prestiżu
    -dodane menu developerskie do testowania
    -dodany bank ( na razie nie dzialający )
    -poprawionych parę błędów
    -kosmetyka

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 #include <windows.h>
 #include <cstdio>
 #include <cstdlib>
+#include <conio.h>
 
 using namespace std;
 
 string nick, nazwisko, miasto;
-int kasa, punkty, ktorydzien, wybor, iledni, cnarkotyk[6], inarkotyk[6], nrsave, ile, ktory, hp;
+int kasa, respekt, ktorydzien, iledni, cnarkotyk[6], inarkotyk[6], ile, ktory, hp;
 
 //kolory
-
 void csilver() //srebrny
 {
     HANDLE hOut;
@@ -48,17 +48,7 @@ void cnormal() //szary
 
 //FUNKCJE GRY
 
-void smierc()
-{
-    system("cls");
-    cout<<"      -------------------------------------------- "<<endl;
-    cout<<"     |               JESTES MARTWY                |"<<endl;
-    cout<<"     |         twoj wynik zostal zapisany         |"<<endl;
-    cout<<"      -------------------------------------------- "<<endl;
-    system("pause");
-}
-
-void podroz()
+void journey() //to other cities
 {
 string miastobuf;
     while(true)
@@ -83,7 +73,7 @@ string miastobuf;
         }
 }
 
-void gotoxyd(int x, int y) //DYNAMICZNE
+void gotoxyd(int x, int y) //go to x/y dynamic
 {
 
     if (iledni>=100)
@@ -100,7 +90,7 @@ void gotoxyd(int x, int y) //DYNAMICZNE
     SetConsoleCursorPosition (GetStdHandle (STD_OUTPUT_HANDLE), c);
 }
 
-void gotoxys(int xx, int yy) //STATYCZNE
+void gotoxys(int xx, int yy) //go to x/y static
 {
     COORD c;
     c.X = xx-1;
@@ -108,20 +98,40 @@ void gotoxys(int xx, int yy) //STATYCZNE
     SetConsoleCursorPosition (GetStdHandle (STD_OUTPUT_HANDLE), c);
 }
 
-void gra() // JUZ PRZEFORMATOWANA
+void gameover()
 {
-bool wyjscie;
-int x;
-if (hp<=0)
-{
-    smierc();
+    system("cls");
+    gotoxys(15, 1);cout<<" ----------------------------------------------"<<endl;
+    gotoxys(15, 2);cout<<"|                UPLYNAL CZAS GRY              |"<<endl;
+    gotoxys(15, 3);cout<<"| Twoj wynik zostal zapisany na liste rekordow |"<<endl;
+    gotoxys(15, 4);cout<<"| Nick: "; csilver(); cout<<nick; gotoxys (62, 4); cnormal();  cout<<"|"<<endl;
+    gotoxys(15, 5);cout<<"| Respekt: "; cgreen(); cout<<respekt; gotoxys (62,5); cnormal(); cout<<"|"<<endl;
+    gotoxys(15, 6);cout<<"| Kasa: "; cgold(); cout<<kasa; gotoxys (62,6); cnormal(); cout<<"|"<<endl;
+    gotoxys(15, 7);cout<<" ----------------------------------------------"<<endl;
+    system("pause");
 }
+
+void yrdead()
+{
+    system("cls");
+    gotoxys(15, 1);cout<<" ----------------------------------------------"<<endl;
+    gotoxys(15, 2);cout<<"|                JESTES MARTWY                 |"<<endl;
+    gotoxys(15, 3);cout<<"| Twoj wynik zostal zapisany na liste rekordow |"<<endl;
+    gotoxys(15, 4);cout<<"| Nick: "; csilver(); cout<<nick; gotoxys (62, 4); cnormal();  cout<<"|"<<endl;
+    gotoxys(15, 5);cout<<"| Respekt: "; cgreen(); cout<<respekt; gotoxys (62,5); cnormal(); cout<<"|"<<endl;
+    gotoxys(15, 6);cout<<"| Kasa: "; cgold(); cout<<kasa; gotoxys (62,6); cnormal(); cout<<"|"<<endl;
+    gotoxys(15, 7);cout<<" ----------------------------------------------"<<endl;
+    system("pause");
+}
+
+void game() // JUZ PRZEFORMATOWANA
+{
+int x;
+
+
 HANDLE hOut;
 hOut = GetStdHandle( STD_OUTPUT_HANDLE );
-
-
 char wybor1;
-
         do
         {
         cout<<" --------------";
@@ -130,21 +140,20 @@ char wybor1;
         gotoxyd(58, 2); cout<<"Do konca gry "<<iledni-ktorydzien<<" dni|"<<endl;
         cgold(); cout<<"|KASA - "<<kasa;
         csilver(); gotoxys(61, 3); cout<<"Miasto - "<<miasto<<endl;
-        cgreen(); cout<<"|Punkty - "<<punkty<<endl;
+        cgreen(); cout<<"|Respekt - "<<respekt<<endl;
         cred(); cout<<"|Zdrowie -  "<<hp<<endl;
         cnormal();
         cout<<"|Ktory dzien: "<<ktorydzien<<endl;
         cout<<" ---------------"<<endl<<endl;
         cout<<"|Kup dragi - b "<<endl;
         cout<<"|Sprzedaj dragi - s "<<endl;
-        cout<<"|Zmien HP (bedzie szpital) - h"<<endl;
+        cout<<"|Szpital - h"<<endl;
         cout<<"|Zmien miasto - c"<<endl;
         cout<<"|Sklep z przedmiotami - p"<<endl;
+        cout<<"|Bank - f"<<endl;
         cout<<"|Pokaz ile masz dragow - i"<<endl;
         cout<<"|WYJDZ DO MENU - q"<<endl<<endl<<endl;
-        cout<<"wybieram  "; cin>>wybor1;
-
-
+        wybor1=getch();
 
             switch(wybor1)
             {
@@ -182,49 +191,116 @@ char wybor1;
                     cout<<" -----------------------------------------"<<endl;
                     cout<<"wpisz ktory narkotyk chcesz sprzedac  ";
                     cin>>ktory;
-                    cout<<endl<<"Jaki towar opylasz? Masz "<<inarkotyk[ktory]<<" sztuk towaru";
+                    cout<<endl<<"Jaki towar opylasz? Masz "<<inarkotyk[ktory]<<" sztuk towaru"<<endl;
                     cout<<"Opylam ";
                     cin>>ile;
                     inarkotyk[ktory]= inarkotyk[ktory]-ile;
                     kasa= kasa+inarkotyk[ktory]*(cnarkotyk[ktory-1]);
-                    Sleep(2000);
+
 
                 } break;
 
-                case 'h': //ZYCIE
+                case 'h': //SZPITAL
                 {
-                    cout<<"Zmieniasz HP"<<endl;
-                    cin>>hp;
+                    if (hp==100)
+                        {cout<<"Nie przesadzasz chlopie? Idz sie przejsc a mi dupy nie zawracaj!"; Sleep(2500); break;}
+                    system("cls");
+                    int hpplus;
+                    char wybor2;
+                    cred(); cout<<"Zdrowie - "<<hp<<endl; cnormal();
+                    cgold(); cout<<"Kasa - "<<kasa<<endl; cnormal();
+                    cout<<"Witaj w szpitalu, ile pk. zycie chcesz odzyskac? ( max 100 )"<<endl;
+                    cout<<"Im bardziej znany jestes tym wiecej kosztowac bedzie operacja"<<endl;
+                    cout<<"aktualny koszt za 1 pkt zdrowia; "<<2+(1*respekt)<<" zl"<<endl;
+                    cin>>hpplus;
+                    if (hp+hpplus>100)
+                        {cout<<"Nie przesadzasz chlopie? Idz sie przejsc a mi dupy nie zawracaj!"; Sleep(2500); break;}
+                    cout<<"Czy na pewno chcesz uleczyc "<<hpplus<<" pkt zycia za "<<hpplus*(2+(1*respekt))<<" zl? (t/n)"<<endl;
+                    cout<<"|"; cin>>wybor2; cout<<"|"<<endl;
+                    if (wybor2=='t')
+                    {
+                        hp=hp+hpplus;
+                        kasa=kasa-hpplus*(2+(1*respekt));
+                    }
+                    else cout<<"jak nie to nie!"; Sleep(2500); break;
+
                 } break;
 
                 case 'c': //PODROZ
                 {
-                   podroz();
+                   journey();
                 }break;
 
                 case 'i': //STAN POSIADANIA
                 {
-                    cout<<"Sprawdzasz kieszenie..."<<endl;
+
+                    cout<<endl<<"Sprawdzasz kieszenie..."<<endl;
+                    cout<<" ----------------------"<<endl;
                     cout<<"Trawka - "<<inarkotyk[1]<<endl;
                     cout<<"Grzybki - "<<inarkotyk[2]<<endl;
                     cout<<"Kokaina - "<<inarkotyk[3]<<endl;
                     cout<<"Hash - "<<inarkotyk[4]<<endl;
-                    cout<<"Amfetamina - "<<inarkotyk[5]<<endl;
-                    Sleep(3000);
+                    cout<<"Amfetamina - "<<inarkotyk[5]<<endl<<endl;
+                    system("pause");
 
 
                 } break;
 
                 case 'p': //SKLEP
                 {
-                    cout<<"Tu bedzie sklep"<<endl;
-                    Sleep(2000);
+                    system("cls");
+                    cout<<"Witaj w sklepie ! "<<endl;
+                    cout<<"Wybierz przedmiot ktory chcesz kupic "<<endl;
+                    cout<<" -----------------------------------------"<<endl;
+                    cout<<"|1)   OGLOSZENIE:                         " <<endl;
+                    cout<<"|2) Prosimy o cierpilowsc !               " <<endl;
+                    cout<<"|3) Czekamy na dostawe czolgow i niedzwiedzi" <<endl;
+                    cout<<"|4)                                       " <<endl;
+                    cout<<"|5)                                       " <<endl;
+                    cout<<" -----------------------------------------"<<endl;
+                    cout<<"Jeszcze zamkniete ;) ";
+                    system("pause");
+
                 } break;
 
-                 case 'q': //WYJSCIE DO MENU
+                case 'f': //BANK
                 {
-                  wyjscie==true;
+                    system("cls");
+                    cout<<"Witaj w Banku Amber Gold L.T.D! "<<endl;
+                    cout<<"...Tu na razie jest sciernisko, ale bedzie san francisco... "<<endl;
+                    cout<<" -----------------------------------------"<<endl;
+                    cout<<"|1)   OGLOSZENIE:                         " <<endl;
+                    cout<<"|2) Zamkniete bo tak.                     " <<endl;
+                    cout<<"|3)                                       " <<endl;
+                    cout<<"|4)                                       " <<endl;
+                    cout<<"|5)                                       " <<endl;
+                    cout<<" -----------------------------------------"<<endl;
+                    cout<<"Jeszcze zamkniete ;) ";
+                    system("pause");
+
                 } break;
+
+                case 'q': //WYJSCIE DO MENU
+                {
+                } break;
+
+                case 'x': //DEVELOPER MENU
+                {
+                    cout<<"Menu Developerskie, jesli jestes graczem to pogratuluj sobie"<<endl;
+                    cout<<"Aktualnie masz "<< hp<<" pkt zrowia"<<endl;
+                    cout<<"zmien ilosc hp"<<endl;
+                    cin>>hp;
+                    cout<<"Aktualnie masz "<< kasa<<" zl w kieszeni"<<endl;
+                    cout<<"zmien ilosc kasy"<<endl;
+                    cin>>kasa;
+                    cout<<"Aktualnie jest "<<ktorydzien<<" dzien gry"<<endl;
+                    cout<<"Zmien dzien"<<endl;
+                    cin>>ktorydzien;
+                    cout<<"Aktualnie masz "<<respekt<<" respektu"<<endl;
+                    cout<<"Zmien ilosc respektu"<<endl;
+                    cin>>respekt;
+
+                }break;
 
                 default: //WYBIERZ POPRAWNA OPCJE
                 {
@@ -233,18 +309,28 @@ char wybor1;
                     system("cls");
                 }break;
             }
+
+
             if (hp<=0)
-            {
-                    smierc();
-            }
+                {
+                yrdead();
+                wybor1='q';
+                }
+
+            if (iledni-ktorydzien<=0)
+                {
+                gameover();
+                wybor1='q';
+                }
             system("cls");
-        }while (wyjscie==true);
+        }while (wybor1!='q');
 }
 
-void zapisz()
+void newprofile()
 {
     string miasto1;
     cout<<endl<<endl;
+    cout<<"   NOWY PROFIL"<<endl;
     cout<<"----------------"<<endl;
     cout<< "Podaj nick: "; cin>>nick;
     cout<< "Wpisz liczbe dni gry "; cin>>iledni;
@@ -254,22 +340,17 @@ void zapisz()
     //ios::out wyprowadü z programu do pliku
     hp =100;
     kasa=500;
-    punkty=0;
+    respekt=0;
     ktorydzien=1;
     miasto1="Wroclaw";
     plik<<nick<<endl;
     plik<<kasa<<endl;
-    plik<<punkty<<endl;
+    plik<<respekt<<endl;
     plik<<ktorydzien<<endl;
     plik<<iledni<<endl;
     plik<<miasto1<<endl;
     plik<<hp<<endl;
     plik.close(); // zamkniecie pliku
-
-    /*cout<<endl<<endl<<"nick - "<<nick<<endl;
-    cout<<"kasa - "<<kasa<<endl;
-    cout<<"punkty - "<<punkty<<endl;
-    cout<<"dlogosc gry w dniach: "<<iledni<<endl;*/
 
     cout<<endl<<endl;
     cout<<"--------------------------------------------"<<endl;
@@ -278,15 +359,15 @@ void zapisz()
     cout<<"--------------------------------------------"<<endl<<endl<<endl;
     system("pause");
     system("cls");
-    gra();
+    game();
 }
 
-void ceny()
+void prices() //na razie puste
 {
 
 }
 
-void wczytaj()
+void load()
 {
 
 
@@ -298,14 +379,14 @@ void wczytaj()
        Sleep(2000);
        exit (0);
     }
-
+    else{
     plikceny>>cnarkotyk[0];
     plikceny>>cnarkotyk[1];
     plikceny>>cnarkotyk[2];
     plikceny>>cnarkotyk[3];
     plikceny>>cnarkotyk[4];
     plikceny.close();
-
+    }
     fstream plik;
     plik.open("save.txt", ios::in);
 
@@ -313,12 +394,11 @@ void wczytaj()
     {
        cout<<"plik nie dziala, najlepiej stworz nowy!";
        Sleep(2000);
-       exit (0);
     }
-
+    else{
     plik>>nick;
     plik>>kasa;
-    plik>>punkty;
+    plik>>respekt;
     plik>>ktorydzien;
     plik>>iledni;
     plik>>miasto;
@@ -333,35 +413,34 @@ void wczytaj()
 
     system("pause");
     system("cls");
-    gra();
+    game();
+    }system("cls");
 }
 
 void intro()
 {
-
-   cout<<" ##::::'##::::'###::::'##::::'##:'########::'####:'##::::::'##::::::::::'###:::"<<endl; Sleep(200);
-   cout<<" ###::'###:::'## ##:::. ##::'##:: ##.... ##:. ##:: ##:::::::##:::::::::'## ##::"<<endl; Sleep(200);
-   cout<<" ####'####::'##:. ##:::. ##'##::: ##:::: ##:: ##:: ##:::::::##::::::::'##:. ##:"<<endl; Sleep(200);
-   cout<<" ## ### ##:'##:::. ##:::. ###:::: ##:::: ##:: ##:: ##:::::::##:::::::'##:::. ##"<<endl; Sleep(200);
-   cout<<" ##. #: ##: #########::: ## ##::: ##:::: ##:: ##:: ##:::::::##::::::: #########"<<endl; Sleep(200);
-   cout<<" ##:.:: ##: ##.... ##:: ##:. ##:: ##:::: ##:: ##:: ##:::::::##::::::: ##.... ##"<<endl; Sleep(200);
-   cout<<" ##:::: ##: ##:::: ##: ##:::. ##: ########::'####: ########:########: ##:::: ##"<<endl; Sleep(200);
+   cout<<" ##::::'##::::'###::::'##::::'##:'########::'####:'##::::::'##::::::::::'###:::"<<endl; Sleep(100);
+   cout<<" ###::'###:::'## ##:::. ##::'##:: ##.... ##:. ##:: ##:::::::##:::::::::'## ##::"<<endl; Sleep(100);
+   cout<<" ####'####::'##:. ##:::. ##'##::: ##:::: ##:: ##:: ##:::::::##::::::::'##:. ##:"<<endl; Sleep(100);
+   cout<<" ## ### ##:'##:::. ##:::. ###:::: ##:::: ##:: ##:: ##:::::::##:::::::'##:::. ##"<<endl; Sleep(100);
+   cout<<" ##. #: ##: #########::: ## ##::: ##:::: ##:: ##:: ##:::::::##::::::: #########"<<endl; Sleep(100);
+   cout<<" ##:.:: ##: ##.... ##:: ##:. ##:: ##:::: ##:: ##:: ##:::::::##::::::: ##.... ##"<<endl; Sleep(100);
+   cout<<" ##:::: ##: ##:::: ##: ##:::. ##: ########::'####: ########:########: ##:::: ##"<<endl; Sleep(100);
     gotoxys(64, 25); cout<<"by Bearshire";
-   Sleep(4000);
+   Sleep(3000);
    system("cls");
 }
 
 int main() // JUZ PRZEFORMATOWANA
 {
+char wybor;
 intro();
     while(true)
     {
 
         cout<<"------------------------------------------------------"<<endl;
-        cout<<"Witaj w MAXDILLA ver. 0.3.5 uposledzona by";
-        cgold();
-        cout<<" Bearshire"<<endl;
-        cnormal();
+        cout<<"Witaj w MAXDILLA ver. 0.4 uposledzona by";
+        cgold(); cout<<" Bearshire"<<endl; cnormal();
         cout<<"------------------------------------------------------"<<endl<<endl;
 
         cout<<"Menu"<<endl;
@@ -369,24 +448,25 @@ intro();
         cout<<"2. Kontynuj gre"<<endl;
         cout<<"3. Zasady"<<endl;
         cout<<"4. O grze"<<endl;
-        cout<<"5. Wyjdz z gry"<<endl;
-        cout<<"wybieram  "; cin>>wybor;
-        cout<<endl;
+        cout<<"5. TOP 10"<<endl;
+        cout<<"6. Wyjdz z gry"<<endl;
+        wybor=getch();
 
         switch(wybor)
         {
-            case 1:
+            case '1':
             {
-                zapisz();
+                newprofile();
             }break;
 
-            case 2:
+            case '2':
             {
-                wczytaj();
+                load();
             } break;
 
-            case 3:
+            case '3':
             {
+                system("cls");
                 cout<<endl<<"Witaj, wiesz co jest celem gry? Wzbogacenie sie oczywiscie - byles za glupi by dostac sie na studia, a jednoczesnie za leniwy by zdobyc dobry fach.";
                 cout<<" Jestes zatem dilerem, - zakala spoleczenstwa.";
                 cout<<" Nie przejmuj sie jednak - jesli przezyjesz i bedziesz wystarczajaco sprytny to zostaniesz";
@@ -401,8 +481,9 @@ intro();
                 system("cls");
             }break;
 
-            case 4:
+            case '4':
             {
+                system("cls");
                 cout<<"Po pierwsze dzieki dla Miroslawa Zelenta, jak i wielu nieocenioncych ludzi w "<<endl;
                 cout<<"internecie ktorzy poswiecali czas by przekazywac wiedze o programowaniu."<<endl;
                 cout<<"Prace nad nia zaczely sie 3 wrzesnia, wieksze zmiany wprowadzono 17 wrzesnia"<<endl;
@@ -422,7 +503,15 @@ intro();
                 system("cls");
             }break;
 
-            case 5:
+            case '5':
+            {
+                system("cls");
+                gotoxys(31,1); cout<<"Tutaj bedzie top 10"<<endl;
+                Sleep(2000);
+                system("cls");
+            }break;
+
+            case '6':
             {
                 return 0;
             }
